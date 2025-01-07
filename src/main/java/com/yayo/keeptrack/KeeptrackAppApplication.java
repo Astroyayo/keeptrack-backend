9bdbd3e21dd5f2bf6486f51aeb43209d9ba3d40a
GerardBR3270 <lugo044@gmail.com>
2025-01-06 23:15:14 -0600

    Initial commit

---
+++ src/main/java/com/yayo/keeptrack/KeeptrackAppApplication.java
@@ -0,0 +1,13 @@
+package com.yayo.keeptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeptrackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeptrackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yayo/keeptrack/KeeptrackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yayo.keeptrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeptrackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
